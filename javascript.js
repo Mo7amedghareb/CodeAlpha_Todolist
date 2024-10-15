6dff5da3b86f061af1f1638d70cdf271bef5a8ac
Mohamed_Ghareb <mo7amed.ghareb13@gmail.com>
2024-10-15 23:06:19 +0300

    edit on the code make alert messages and update button and make it responsive

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 let input =document.querySelector(".input"); 
+let myid=0;
 let add =document.querySelector(".add");
 let tasks =document.querySelector(".tasks");
 let arrayOfTasks=[]; // array that stores the tasks
-
+let btnupdate=document.querySelector(".btnupdate");
 // if local storage have items get these items
 if(localStorage.getItem("tasks")){
     arrayOfTasks =JSON.parse(localStorage.getItem("tasks"));
@@ -14,8 +15,13 @@ getdataFromLocalStorage();  // draw this items in the page
 //add task button
 add.onclick=function(){
     if(input.value != ""){
-        addTasksToArray(input.value);
+        if(myid<=arrayOfTasks.length){
+            addTasksToArray(myid,input.value);
+        }
+        
         input.value=""; // after add the task i clear the input field
+        window.alert("task was added successfuly");
+        myid++;
     }
 }
 
@@ -27,6 +33,7 @@ tasks.addEventListener("click" ,(e)=>{
     if(e.target.classList.contains("del")){
         deletetask(e.target.parentElement.getAttribute("data-id"));
         e.target.parentElement.remove();
+        window.alert("task removed");
     }
 
     //when click on task it will be completed
@@ -34,14 +41,45 @@ tasks.addEventListener("click" ,(e)=>{
         completedOrNot(e.target.getAttribute("data-id"));
         e.target.classList.toggle("done");
     }
+    if(e.target.classList.contains("upd")){
+        updatetask(e.target.parentElement.getAttribute("data-id"));
+    }
+
+    if(e.target.classList.contains("btnupdate")){
+        btnupdatetask();
+    }
 })
+let x=0;
+function updatetask(taskid){
+    for(let i=0 ;i<arrayOfTasks.length;i++){
+        if(arrayOfTasks[i].id == taskid){
+            x=arrayOfTasks[i];
+            input.value=arrayOfTasks[i].title;
+        }
+    }
+    addTasksToLocalStorage(arrayOfTasks);
+    addElementsToTasksDiv(arrayOfTasks);
+    
+}
+
+
+function btnupdatetask(){
+
+    arrayOfTasks[x.id].title=input.value;
+    addTasksToLocalStorage(arrayOfTasks);
+    addElementsToTasksDiv(arrayOfTasks);
+    input.value="";
+}
+
+
+
 
 
 
 // function that add tasks to array and to the local storage then draw it in the page
-function addTasksToArray(taskText){
+function addTasksToArray(id,taskText){
     const mytask={ // object that contains task data
-        id : Date.now(),
+        id : id,
         title : taskText,
         completed : false,
     };
@@ -58,6 +96,7 @@ function addElementsToTasksDiv(arrayOfTasks){
     arrayOfTasks.forEach((element) => {
         let div=document.createElement("div") ;
         div.className="task";
+
         // if enter on the task
         if(element.completed){
             div.className="task done";
@@ -65,15 +104,23 @@ function addElementsToTasksDiv(arrayOfTasks){
         div.setAttribute("data-id",element.id); 
         div.appendChild(document.createTextNode(element.title));
 
+        let update=document.createElement("button");
+        update.className="upd";
+        update.appendChild(document.createTextNode("update"));
+        div.appendChild(update);
+
         let span=document.createElement("span");
         span.className="del";
-        span.appendChild(document.createTextNode("delete  x"));
-
+        span.appendChild(document.createTextNode("delete"));
         div.appendChild(span);
 
+        
         tasks.appendChild(div);
     });
 }
+
+
+
 
 
 
@@ -98,11 +145,17 @@ function deletetask(taskid){
 }
 
 
+
+
+
 //function that save the status of task if completed or not
 function completedOrNot(taskid){
     for(let i=0 ;i<arrayOfTasks.length;i++){
         if(arrayOfTasks[i].id ==taskid){
             arrayOfTasks[i].completed ==false? (arrayOfTasks[i].completed=true) : (arrayOfTasks[i].completed=false);
+            if(arrayOfTasks[i].completed==true){
+                window.alert("task completed");
+            }
         }
     }
     addTasksToLocalStorage(arrayOfTasks);
@@ -113,4 +166,5 @@ function deleteall(){
     tasks.innerHTML="";
     localStorage.clear();
     arrayOfTasks=[];
+    window.alert("all tasks have been deleted");
 }
